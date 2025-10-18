43e4c1105461d215ab24d972512004a7c141eaca
Ali-Shaikh-Qasem <aliqasem@DESKTOP-3914IL5.localdomain>
2025-10-18 22:29:30 +0300

    driving our 3D model again

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -1,44 +1,45 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 from launch_ros.actions import Node
+
+
 
 def generate_launch_description():
 
-    package_name = 'articubot_one'  # Your package with URDF and rsp.launch.py
 
-    # 1️⃣ Launch robot_state_publisher (same as old)
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='articubot_one' 
+
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-                get_package_share_directory(package_name),
-                'launch',
-                'rsp.launch.py'
-            )
-        ),
-        launch_arguments={'use_sim_time': 'true'}.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # 2️⃣ Launch Gazebo Harmonic (ros_gz_sim style)
-    gazebo = ExecuteProcess(
-        cmd=['gz', 'sim', '-v', '4', '--render-engine', 'ogre', '-r', 'empty.sdf'],
-        output='screen'
-    )
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
 
-    # 3️⃣ Spawn your robot in Gazebo
-    spawn_entity = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-topic', 'robot_description',  # Uses the topic published by robot_state_publisher
-            '-name', 'my_bot'
-        ],
-        output='screen'
-    )
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
 
-    # Return LaunchDescription with all three
+
+
+    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
